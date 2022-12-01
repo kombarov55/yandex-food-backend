817af17c0ffd65c849028697c1cee4906f86eb94
kombarov <kombarov@itopcase.ru>
2022-12-01 07:45:05 +0300

    конфиги

--- config/app_config.py
+++ config/app_config.py
@@ -1,3 +1,4 @@
 headless = True
 screenshot_allowed = True
 screenshot_dir = "./screenshots"
+db_path = "/opt/yandex-food.db"
--- config/database.py
+++ config/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./sql_app.db"
+from config import app_config
+
+DATABASE_URL = "sqlite:///{}".format(app_config.db_path)
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
