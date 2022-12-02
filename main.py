be5714f608fbd425d2c648e169538bc7e94b7a54
kombarov <kombarov@itopcase.ru>
2022-12-02 14:16:17 +0300

    конфигурация https

--- main.py
+++ main.py
@@ -69,12 +69,12 @@ app.add_middleware(
 
 
 def is_prod():
-    # if len(sys.argv) == 0:
-    #     return False
-    # if sys.argv[0] == "prod":
-    #     return True
-    # return False
-    return True
+    print(sys.argv)
+    if len(sys.argv) < 2:
+        return False
+    if sys.argv[1] == "prod":
+        return True
+    return False
 
 
 if __name__ == "__main__":
