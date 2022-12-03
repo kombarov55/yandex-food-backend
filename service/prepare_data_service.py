a0b365f49651afd7c7f96308008db10ba6b19e17
kombarov <kombarov@itopcase.ru>
2022-12-03 12:49:50 +0300

    лучшая еда

--- service/prepare_data_service.py
+++ service/prepare_data_service.py
@@ -20,5 +20,5 @@ def load_data():
     vo = xlsx_request_repository.create(session, "Цезарь", True)
 
     with sync_playwright() as p:
-        page = p.chromium.launch(headless=False).new_page()
+        page = p.chromium.launch(headless=True).new_page()
         yandex_food_parser.parse_data_and_save_to_db(page, session, vo)
